1103677010aa3b33343b0000e371d20c3de879c3
DOCTOR_RABB <doctor.rabb@mail.ru>
2016-02-20 12:36:32 +0300

    src

--- modules/bot.py
+++ modules/bot.py
@@ -16,15 +16,17 @@ class BotM:
 	def __init__ (self, client):
 	        self.client = client
 
-	def start_message_flood (self, login):
+	def start_message_flood (self, login, delay):
     		mes = ''
-    		for i in range (11): mes += str (randint (0, 1000000))*i
     		while True:
+		   for i in range (11): mes += str (randint (0, 1000000))*i
         	   try:
-             		if login is not 'all': 
+             		if login != 'all': 
  			     self.client.SendMessage (login, mes)
+			     sleep (delay)
 			else: 
-			     for i in self.client.Friends: self.client.SendMessage (i.Handle)
+			     for i in self.client.Friends: self.client.SendMessage (i.Handle, mes)
+			     sleep (delay)
 	    		print YES + 'Flood Sent'
                    except KeyboardInterrupt:
             		opt (self.client)
@@ -33,10 +35,16 @@ class BotM:
 	def start_call_flood (self, login, delay):
     		while True:
         	    try:
-            		call = self.client.PlaceCall (login)
-			sleep (delay)
-			call.Finish ()
-	    		print YES + 'Flood Sent'
+			if login != 'all':
+            		    call = self.client.PlaceCall (login)
+			    sleep (delay)
+			    call.Finish ()
+			else:
+			    for i in self.client.Friends:
+				call = self.client.PlaceCall (i.Handle)
+				sleep (delay)
+				call.Finish ()
+	    		    print YES + 'Flood Sent'
         	    except Exception as e:
             		if e == KeyboardInterrupt:
 				 opt (self.client)
